c81213f6caf556de268ef68bd32f5e42aab20cbf
mostafa <mostafameerzad@gmail.com>
2025-02-16 15:12:50 +0430

    feat(Url model): define url model

---
+++ backend/models/Url.ts
@@ -0,0 +1,24 @@
+import { Document, Schema, model } from "mongoose";
+
+export interface IUrl extends Document {
+  originalUrl: string;
+  shortUrl: string;
+  user: Schema.Types.ObjectId;
+  clicks: number;
+  createdAt: Date;
+}
+
+const urlSchema = new Schema<IUrl>(
+  {
+    originalUrl: { type: String, required: true },
+    shortUrl: { type: String, required: true, unique: true },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    clicks: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+const User = model<IUrl>("Url", urlSchema);
+
+export default User;
