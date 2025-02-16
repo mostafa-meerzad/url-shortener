a4221d79533f190bab0909bfaa8c0b4d41c4c642
mostafa <mostafameerzad@gmail.com>
2025-02-16 13:52:50 +0430

    feat(User model): define user model

---
+++ backend/models/user.ts
@@ -0,0 +1,39 @@
+import mongoose, { Document } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  name: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
+  password: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = bcrypt.getSalt("10");
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
