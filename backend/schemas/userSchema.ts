e1c0d359d95a4395441475e0e41543aa669816da
mostafa <mostafameerzad@gmail.com>
2025-02-20 11:02:54 +0430

    fix( userSchema ): fix the user login schema

--- backend/schemas/userSchema.ts
+++ backend/schemas/userSchema.ts
@@ -7,7 +7,6 @@ const userRegisterSchema = z.object({
 });
 
 const userLoginSchema = z.object({
-  name: z.string().min(3, "Name must be at least 3 characters long").max(50),
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
 });
