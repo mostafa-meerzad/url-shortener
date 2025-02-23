c6a4e25092482bb1a93abdfa881d87e2c0cad719
mostafa <mostafameerzad@gmail.com>
2025-02-23 22:20:57 +0430

    refactor( images ): move decorative images to public

--- frontend/src/assets/images/index.ts
+++ frontend/src/assets/images/index.ts
@@ -1,6 +1,4 @@
 import logo from "./logo.svg";
-import bgShortenDesktop from "./bg-shorten-desktop.svg";
-import bgShortenMobile from "./bg-shorten-mobile.svg";
 import illustrationWorking from "./illustration-working.svg";
 
-export { logo, bgShortenDesktop, bgShortenMobile, illustrationWorking };
+export { logo, illustrationWorking };
