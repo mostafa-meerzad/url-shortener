45dbe59b19141e8c8b4ef48161287b6e80b233f7
mostafa <mostafameerzad@gmail.com>
2025-02-23 08:44:46 +0430

    refactor( assets ): remove template assets and add project assets

---
+++ frontend/src/assets/images/index.ts
@@ -0,0 +1,6 @@
+import logo from "./logo.svg";
+import bgShortenDesktop from "./bg-shorten-desktop.svg";
+import bgShortenMobile from "./bg-shorten-mobile.svg";
+import illustrationWorking from "./illustration-working.svg";
+
+export { logo, bgShortenDesktop, bgShortenMobile, illustrationWorking };
