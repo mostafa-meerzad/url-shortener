8b5461ceaa37af949d94c89b5b26271985fe14a8
mostafa <mostafameerzad@gmail.com>
2025-02-24 20:29:02 +0430

    refactor( navbar ): optimize navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,28 +1,19 @@
-import classNames from "classnames";
 import { logo } from "../assets/images";
+import NavLink, { LinkItem } from "./NavLink";
 
 const Header = () => {
-  const links = [
+  const links: LinkItem[] = [
     { label: "login", href: "/" },
-    { label: "sign up", href: "/" },
+    { label: "sign up", href: "/", variant: "primary" },
   ];
 
   return (
     <header className="p-6 md:p-10">
       <nav className="flex justify-between items-center ">
         <img src={logo} alt="URL Shortener" className=" h-7 " />
-
         <ul className="flex gap-3">
-          {links.map(({ label, href }) => (
-            <li
-              className={classNames({
-                " py-2 px-4 rounded-4xl capitalize text-gray-600 font-medium hover:opacity-65 ":
-                  true,
-                "bg-cyan text-white": label === "sign up",
-              })}
-            >
-              <a href={href}>{label}</a>
-            </li>
+          {links.map((link) => (
+            <NavLink {...link} key={link.label} />
           ))}
         </ul>
       </nav>
