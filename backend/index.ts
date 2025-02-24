594e2fff6cc97e6b43471a8e9491deb51f24bd9f
mostafa <mostafameerzad@gmail.com>
2025-02-24 21:10:46 +0430

    feat( server ): allow cors for localhost:5173

--- backend/index.ts
+++ backend/index.ts
@@ -2,10 +2,12 @@ import express, { NextFunction, Request, Response } from "express";
 import "./startup/connectToDB";
 import { auth } from "./routes/auth";
 import { shorten } from "./routes/shorten";
+import cors from "cors";
 
 const app = express();
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
-// Handle invalid JSON Objects 
+// Handle invalid JSON Objects
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof SyntaxError && "body" in err) {
     res.status(400).json({ error: "Invalid JSON format" });
@@ -15,7 +17,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 });
 
 app.use("/api/auth", auth);
-app.use("/api/urls", shorten)
+app.use("/api/urls", shorten);
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to user URL Shortening service 🚀" });
   return;
